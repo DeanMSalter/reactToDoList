602614ff26eeb55ca0aec3019066a1be65c874aa
DeanMSalter <Dean.M.Salter@gmail.com>
2022-01-19 06:57:48 +0000

    add tasks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route } from "react-router-dom";
+import {useState} from "react";
 
+const staticTasks = [
+    "Get Milk",
+    "Walk Dog",
+    "Do other things"
+]
 
-
+function NoteEntry(props) {
+    const [task, setTask] = useState("");
+    return (
+        <div className="taskAdding">
+            <input
+                type="text"
+                value={task}
+                onChange={event => setTask(event.target.value )}
+                placeholder="New Task.."
+                required
+            />
+            <input className="btnTaskAdd" type="button" value="Add" onClick={() => {
+                props.addNote(task)
+            }}/>
+        </div>
+    );
+}
 
 function App() {
+  const [tasks, setTasks] = useState(staticTasks);
+
+  const updateTasks =  (task) => {
+      console.log("hereeee")
+      setTasks(tasks.concat(task))
+  }
+
   return (
     <div className="App">
       <section>
-          <div class="taskAdding">
-              <textarea></textarea>
-          </div>
+          <NoteEntry addNote={updateTasks}></NoteEntry>
           <div class="listHolder">
               <ol>
-                  <li>Get milk</li>
-                  <li>Get milk</li>
-                  <li>Get milk</li>
-                  <li>Get milk</li>
-                  <li>Get milk</li>
+                  {tasks.map(task =>
+                    <li>{task}</li>
+                  )}
               </ol>
           </div>
       </section>
